b8fdc97222649ec857c338f77a3284e2b37c044e
Pedro Vitor Rodrigues da Silva <pedrocavt@gmail.com>
2025-05-26 15:29:12 -0300

    iniciando cronometro

--- vue-3/alura-tracker/src/components/FormularioTarefa.vue
+++ vue-3/alura-tracker/src/components/FormularioTarefa.vue
@@ -13,17 +13,17 @@
         />
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-center-space-between">
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
           <section>
-              <strong>00:00:00</strong>
+              <strong>{{ tempoDecorrido }}</strong>
           </section>
-          <button class="button">
+          <button class="button" @click="iniciar">
               <span class="icon">
                   <i class="fas fa-play"></i>
               </span>
               <span>play</span>
           </button>
-          <button class="button">
+          <button class="button" @click="finalizar">
               <span class="icon">
                   <i class="fas fa-stop"></i>
               </span>
@@ -39,6 +39,26 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-    name: 'FormularioTarefa'
+  name: 'FormularioTarefa',
+    data() {
+      return {
+        tempoEmSegundos: 0
+      }
+    },
+    computed: {
+      tempoDecorrido() : string {
+        return new Date(this.tempoEmSegundos * 1000).toISOString().substr(11, 8)
+      }
+    },
+    methods: {
+      iniciar() {
+        setInterval(() => {
+          this.tempoEmSegundos += 1
+        }, 1000)
+      },
+      finalizar() {
+        console.log('finalizando')
+      }
+    }
 })
 </script>
