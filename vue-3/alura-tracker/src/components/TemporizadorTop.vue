fc07dab9c2a330fbdc4a90bd8f27f657586ef535
Pedro Vitor Rodrigues da Silva <pedrocavt@gmail.com>
2025-05-27 17:59:18 -0300

    botao componenet

--- vue-3/alura-tracker/src/components/TemporizadorTop.vue
+++ vue-3/alura-tracker/src/components/TemporizadorTop.vue
@@ -1,24 +1,15 @@
 <template>
-    <div class="is-flex is-align-items-center is-justify-content-space-between">
-        <CronometroTop :tempoEmSegundos="tempoEmSegundos"/>
-        <button class="button" @click="iniciar" :disabled="cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-        </button>
-        <button class="button" @click="finalizar" :disabled="!cronometroRodando">
-            <span class="icon">
-                <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-        </button>
-    </div>
+    <section class="is-flex is-align-items-center is-justify-content-space-between">
+      <CronometroTop :tempoEmSegundos="tempoEmSegundos"/>
+      <BotaoTop @clicado="iniciar" icone="fas fa-play" texto="play" :desabilitado="cronometroRodando" />
+      <BotaoTop @clicado="finalizar" icone="fas fa-stop" texto="stop" :desabilitado="!cronometroRodando" />
+    </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
 import CronometroTop from './CronometroTop.vue';
+import BotaoTop from './BotaoTop.vue';
 
 
 export default defineComponent({
@@ -46,7 +37,8 @@ export default defineComponent({
     }
   },
   components: {
-    CronometroTop
+    CronometroTop,
+    BotaoTop
   }
 })
 </script>
