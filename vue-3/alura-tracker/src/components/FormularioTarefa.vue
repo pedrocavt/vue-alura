8a7f9d27dff3d12df0f15964c3ba31c947108687
Pedro Vitor Rodrigues da Silva <pedrocavt@gmail.com>
2025-05-27 17:33:14 -0300

    temporizador component

--- vue-3/alura-tracker/src/components/FormularioTarefa.vue
+++ vue-3/alura-tracker/src/components/FormularioTarefa.vue
@@ -13,21 +13,7 @@
         />
       </div>
       <div class="column">
-        <div class="is-flex is-align-items-center is-justify-content-space-between">
-          <CronometroTop :tempoEmSegundos="tempoEmSegundos"/>
-          <button class="button" @click="iniciar">
-              <span class="icon">
-                  <i class="fas fa-play"></i>
-              </span>
-              <span>play</span>
-          </button>
-          <button class="button" @click="finalizar">
-              <span class="icon">
-                  <i class="fas fa-stop"></i>
-              </span>
-              <span>stop</span>
-          </button>
-        </div>
+        <TemporizadorTop />
       </div>
     </div>
   </div>
@@ -35,29 +21,12 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import CronometroTop from './CronometroTop.vue';
+import TemporizadorTop from './TemporizadorTop.vue';
 
 export default defineComponent({
   name: 'FormularioTarefa',
-  data() {
-    return {
-      tempoEmSegundos: 0,
-      cronometro: 0
-    }
-  },
-  methods: {
-    iniciar() {
-      this.cronometro = setInterval(() => {
-        this.tempoEmSegundos += 1
-      }, 1000)
-    },
-    finalizar() {
-      console.log('teste')
-      clearInterval(this.cronometro)
-    }
-  },
   components: {
-    CronometroTop
+    TemporizadorTop
   }
 })
 </script>
